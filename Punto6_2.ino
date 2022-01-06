6d9adf222ab5496a7e03c96dd9d21c11a2859a51
rolopu1 <55992854+rolopu1@users.noreply.github.com>
2022-01-06 02:30:09 +0100

    Corrección de la resta

--- Punto6_2.ino
+++ Punto6_2.ino
@@ -11,13 +11,13 @@
 #include "Time.h"
 
 const char* ntpServer = "pool.ntp.org";
-const long gmtOffset_sec = 0;
+const long gmtOffset_sec = 3600;
 const int daylightOffset_sec = 3600;
 bool resta= false;
 
 // Replace with your network credentials
-const char* ssid = "ONO6961";
-const char* password = "YK6dR5eCsS55";
+const char* ssid = "vodafone9320";//"Rodrigo";//""ONO6961";
+const char* password = "QWNLTLDTZHFWNH";//"12345678";//"YK6dR5eCsS55";
 
 // Create AsyncWebServer object on port 80
 AsyncWebServer server(80);
@@ -40,21 +40,82 @@ void printLocalTime()
 
 String enviarHora(){
   Serial.println("EnviarHora()");
+  
   if(resta){
     return Relativa();
-    resta = false;
   }else{
     printLocalTime();
     String out = String(timeStringBuff);
     return out;
-    
   }
+  
 }
 
 
 
 String Relativa(){
+  printLocalTime();
+  char CadenaA[] = {'0','0',':','0','0',':','0','0','\n'};
+  char CadenaP[] = {'0','0',':','0','0',':','0','0','\n'};
+  char CadenaEnviada[] = {'0','0',':','0','0',':','0','0','\n'};
+  String(timeStringBuff).toCharArray(CadenaA,9);
+  String(SecondsStartBuff).toCharArray(CadenaP,9);
+  char num; 
+  int acarreo = 0;
+  int segundosA = (int(CadenaA[6]-'0')*10) + CadenaA[7]-'0';
+  int segundosP = (int(CadenaP[6]-'0')*10) + CadenaP[7]-'0';
+  int minA = (int(CadenaA[3]-'0')*10) + CadenaA[4]-'0';
+  int minP = (int(CadenaP[3]-'0')*10) + CadenaP[4]-'0';
+  int horA = (int(CadenaA[0]-'0')*10) + CadenaA[1]-'0';
+  int horP = (int(CadenaP[0]-'0')*10) + CadenaP[1]-'0';
+  Serial.println(segundosA);
+  int segundos, minutos, horas ;
+
+  segundos = segundosA - segundosP;
+  minutos = minA - minP;
+  horas = horA-horP;
+  if(segundos < 0){
+    segundos = 60 + segundos;
+     minutos = minutos -1;
+  }
+  if (minutos<0){
+    minutos = 60+minutos;
+    horas = horas -1;
+  }if (horas<0){
+    horas = 24 + horas ;
+  }
+  //Serial.println(horas);
+  //Serial.println(minutos);
+  //Serial.println(segundos);
+  String uni, Sh, Ss, Sm;
+  
+  if(segundos < 10){
+    Ss = '0' + String(segundos);
+  }else{
+    Ss = String(segundos);
+  }
+  if(minutos <10){
+    Sm = '0' + String(minutos);
+  }else{
+    Sm = String(minutos);
+  }
+  if(horas < 10){
+    Sh = '0' + String(horas);
+  }else{
+    Sh = String(horas);
+  }
+  
+  uni = Sh + ':' + Sm + ':' + Ss;
+  //Serial.println(uni);
+  CadenaEnviada[2] = ':';
+  CadenaEnviada[5] = ':';
+
+  //Serial.println(String(CadenaEnviada));
+  String out = String(uni);
+  
+  /*
   int res = int(timeStringBuff)-int(SecondsStartBuff);
+  Serial.println(res);
   int minutos = res/60;
   int horas = minutos/60;
   minutos = minutos - horas*60;
@@ -72,7 +133,8 @@ String Relativa(){
     h = "0"+h;
   }
   String out = h+":"+mini+":"+seg;
-  
+  Serial.println(out);
+  */
   return out;
 }
 
@@ -87,7 +149,14 @@ String processor2(const String& var){
 
 void Arestar(){
   resta = true;
-  strftime(SecondsStartBuff, sizeof(SecondsStartBuff), "%H:%M:%S", &SecondsStart);
+  if(!getLocalTime(&timeinfo)){
+    Serial.println("Failed to obtain time");
+    return;
+  }
+  Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
+  strftime(SecondsStartBuff, sizeof(SecondsStartBuff), "%H:%M:%S", &timeinfo);
+  delay(100);
+  Serial.println(String(SecondsStartBuff));
   return;
 }
 
